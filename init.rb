13601a20c7f38df6bf844c3ecf9a4bea569a09e0
ootr <git@ootr.jp>
2024-11-28 17:43:12 +0900

    init.rbを更新

--- init.rb
+++ init.rb
@@ -1,14 +1,14 @@
 Redmine::Plugin.register :kanban do
   name 'Kanban plugin'
-  author 'Kohei Nomura'
-  description 'Kanban plugin for redmine'
-  version '0.0.11'
-  url 'https://github.com/happy-se-life/kanban'
-  author_url 'mailto:kohei_nom@yahoo.co.jp'
-  
+  author 'ootr'
+  description 'Kanban plugin for redmine 6.x original: https://github.com/happy-se-life/kanban'
+  version '0.0.12'
+  url 'https://github.com/o-tr/kanban'
+  author_url 'https://github.com/o-tr'
+
   # Display application common menu
   menu :application_menu, :display_menu_link, { :controller => 'kanban', :action => 'index' }, :caption => :kanban_menu_caption, :if => Proc.new { User.current.logged? }
-  
+
   # Display menu at project page
   menu :project_menu, :display_menu_link, { :controller => 'kanban', :action => 'index' }, :caption => :kanban_menu_caption, :param => :project_id
 
